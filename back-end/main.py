49a343cf74d310658e13e1686e1b70d84e379990
Ryo <k6.developer@gmail.com>
2024-04-05 17:20:21 +0800

    feat: basic test.

---
+++ back-end/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "FastAPI"}
+
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
--- front-end/App.js
+++ front-end/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Keyboard } from 'react-native';
+
+const onButtonPress = () => {
+  console.log('submit: ');
+  Keyboard.dismiss();
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
+      <TextInput style={styles.input}/>
+      <Button title="送出" style={styles.btn} onPress={() => onButtonPress()} />
     </View>
   );
 }
@@ -16,5 +23,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
   },
 });
